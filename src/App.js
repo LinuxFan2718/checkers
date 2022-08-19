90bb73de3aabd215a9a1c7f61e791aa7017cfe88
Dennis Cahillane <dennis@thirtymadison.com>
2022-08-19 00:49:33 -0700

    make checkerboard fit in viewscreen

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Board from './Board';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <h1>Checkers</h1>
+      <div className='break'></div>
       <Board />
-    </>
+    </div>
   );
 }
 
